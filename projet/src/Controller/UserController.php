01eceb78ccbae71e78c28c0e9cfdf02dd6d2b58e
9MoiioM9 <killian.lespa@gmail.com>
2023-03-28 12:19:27 +0200

    Creation page d'accueil

--- projet/src/Controller/UserController.php
+++ projet/src/Controller/UserController.php
@@ -3,17 +3,17 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+//#[Route('/user', name: '_user')]
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
+    #[Route('/', name: '_user')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        return $this->render("User/view.html.twig");
     }
+
+
 }
