870ebe68b08e2b5635f0032e0f88a627f864ad7a
9MoiioM9 <killian.lespa@gmail.com>
2023-04-07 10:00:29 +0200

    controller USer plus page acceuil ave login

--- projet/src/Controller/UserController.php
+++ projet/src/Controller/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +17,56 @@ class UserController extends AbstractController
     {
         return $this->render("User/view.html.twig");
     }
+
+
+    public function ajouterEnDurAction(EntityManagerInterface $em)
+    {
+        $user_rita = new User();
+        $user_rita
+            ->setLogin("rita")
+            ->setPassword("atir")
+            ->setNom("Zrour")
+            ->setPrenom("Rita")
+            ->setRoles(["client"])
+            ->setDateNaiss(05/05/1990);
+        $em->persist($user_rita);
+
+        $user_gilles = new User();
+        $user_gilles
+            ->setLogin("gilles")
+            ->setPassword("sellig")
+            ->setNom("Subrenat")
+            ->setPrenom("Gilles")
+            ->setRoles(["administrateur"])
+            ->setDateNaiss(05/06/1983);
+        $em->persist($user_gilles);
+
+        $user_sadmin = new User();
+        $user_sadmin
+            ->setLogin("sadmin")
+            ->setPassword("nimdas")
+            ->setNom("Moi")
+            ->setPrenom("Moi")
+            ->setRoles(["super-administrateur"])
+            ->setDateNaiss(05/06/1023);
+        $em->persist($user_sadmin);
+
+        $user_simon = new User();
+        $user_simon
+            ->setLogin("simon")
+            ->setPassword("nomis")
+            ->setNom("Simon")
+            ->setPrenom("Simon")
+            ->setRoles(["client"])
+            ->setDateNaiss(05/06/1213);
+        $em->persist($user_simon);
+
+
+        $em->flush();
+        dump($user_rita);
+
+    }
+
 
 
 }
